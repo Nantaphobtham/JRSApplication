c5b961202b51a6091c185c0ad39671c27f9eef3a
JRSConstruction <jrsconstruction164@gmail.com>
2025-12-07 16:40:54 +0700

    update

--- ProjectManager/ProjectPhaseListRequestsforApproval.Designer.cs
+++ ProjectManager/ProjectPhaseListRequestsforApproval.Designer.cs
@@ -37,10 +37,13 @@
             // searchboxControl1
             // 
             this.searchboxControl1.BackColor = System.Drawing.Color.White;
-            this.searchboxControl1.Location = new System.Drawing.Point(898, 9);
-            this.searchboxControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.searchboxControl1.DefaultFunction = "จัดการบัญชีผู้ใช้";
+            this.searchboxControl1.DefaultRole = "Admin";
+            this.searchboxControl1.Keyword = "";
+            this.searchboxControl1.Location = new System.Drawing.Point(1197, 11);
+            this.searchboxControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.searchboxControl1.Name = "searchboxControl1";
-            this.searchboxControl1.Size = new System.Drawing.Size(650, 50);
+            this.searchboxControl1.Size = new System.Drawing.Size(867, 62);
             this.searchboxControl1.TabIndex = 15;
             // 
             // dtgvRequestApproval
@@ -48,13 +51,13 @@
             this.dtgvRequestApproval.AllowUserToAddRows = false;
             this.dtgvRequestApproval.AllowUserToDeleteRows = false;
             this.dtgvRequestApproval.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgvRequestApproval.Location = new System.Drawing.Point(36, 76);
-            this.dtgvRequestApproval.Margin = new System.Windows.Forms.Padding(2);
+            this.dtgvRequestApproval.Location = new System.Drawing.Point(48, 94);
+            this.dtgvRequestApproval.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dtgvRequestApproval.Name = "dtgvRequestApproval";
             this.dtgvRequestApproval.ReadOnly = true;
             this.dtgvRequestApproval.RowHeadersWidth = 51;
             this.dtgvRequestApproval.RowTemplate.Height = 24;
-            this.dtgvRequestApproval.Size = new System.Drawing.Size(1512, 851);
+            this.dtgvRequestApproval.Size = new System.Drawing.Size(2016, 1047);
             this.dtgvRequestApproval.TabIndex = 14;
             this.dtgvRequestApproval.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgvRequestApproval_CellDoubleClick);
             // 
@@ -62,23 +65,23 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(31, 20);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(41, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 30);
+            this.label1.Size = new System.Drawing.Size(198, 30);
             this.label1.TabIndex = 13;
-            this.label1.Text = "ค้นหาโครงการ";
+            this.label1.Text = "รายการคำขออนุมัติ";
             // 
             // ProjectPhaseListRequestsforApproval
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.Controls.Add(this.label1);
             this.Controls.Add(this.searchboxControl1);
             this.Controls.Add(this.dtgvRequestApproval);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ProjectPhaseListRequestsforApproval";
-            this.Size = new System.Drawing.Size(1584, 954);
+            this.Size = new System.Drawing.Size(2112, 1174);
             ((System.ComponentModel.ISupportInitialize)(this.dtgvRequestApproval)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
